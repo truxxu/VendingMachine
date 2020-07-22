d03559e199ea8a1c51e5da628a143baa25a55142
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-07-22 11:22:21 -0500

    installed icons library

--- src/atoms/CategoryCard.js
+++ src/atoms/CategoryCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const CategoryCard = () => {
   return (
     <View style={styles.container}>
+      <Icon name="rocket" size={30} color="whitesmoke" />
       <Text style={styles.text}>Drinks</Text>
     </View>
   );
@@ -12,6 +14,8 @@ const CategoryCard = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     color: 'whitesmoke',
