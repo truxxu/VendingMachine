46d4580d81f3fec34f20228b3dd6d811d9b411ce
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-07-22 10:54:44 -0500

    implemented navigation between screens

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Categories from './src/screens/Categories';
+import Products from './src/screens/Products';
+import SelectedProduct from './src/screens/SelectedProduct';
+import Payment from './src/screens/Payment';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>Vending Machine App</Text>
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Categories"
+          component={Categories}
+          options={{title: 'Feedr'}}
+        />
+        <Stack.Screen name="Products" component={Products} />
+        <Stack.Screen name="SelectedProduct" component={SelectedProduct} />
+        <Stack.Screen name="Payment" component={Payment} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
